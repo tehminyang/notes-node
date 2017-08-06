2a8d73b5b147be09c8f624028458689740e6f8db
Tim Yang <tim.yang@earnest.com>
2017-08-05 21:49:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+// console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js')
+
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+};
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Delete a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+var command = argv._[0];
+// console.log('Command: ', command);
+
+// console.log('Process: ', process.argv);
+// console.log('Yargs: ', argv);
+
+if (command === 'add') {
+    var note = notes.addNote(argv.title, argv.body);
+    if (note === undefined) {
+        console.log('Note title taken');
+    } else {
+        console.log('Note created');
+        notes.logNote(note);
+    }
+} else if (command === 'list') {
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((note) => notes.logNote(note));
+} else if (command === 'read') {
+    var note = notes.getNote(argv.title);
+    if (note === undefined) {
+        console.log('Note not found');
+    } else {
+        console.log('Note');
+        notes.logNote(note);
+    }
+} else if (command === 'remove') {
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(message);
+} else {
+    console.log('Command not recognized')
+}
+
+
+
+
+// var filteredArray = _.uniq(['tim', 'tim', 1, 1, 2, 3, 4]);
+// console.log(filteredArray);
+
+// var user = os.userInfo();
+
+// var res = notes.addNote();
+// console.log(res);
+
+// console.log('Results: ', notes.add(1,2));
+
+// fs.appendFile('greetings.txt', ` Hello ${user.username}! You are ${notes.age}.`, function(err) {
+//     if (err) {
+//         console.log('Unable to write file');
+//     }
+// });
